fcc2f24a48cefb0af2cbd0f533e9bb62d339eac4
lbenny19 <liya.benny@dxc.com>
2026-06-10 17:10:28 +0530

    Comment out ZOWE_CLI_HOME and ZOWE_CLI_TELEMETRY_OPTOUT
    Comment out environment variable exports for ZOWE_CLI_HOME and ZOWE_CLI_TELEMETRY_OPTOUT.

--- hooks/validate_pr.sh
+++ hooks/validate_pr.sh
@@ -9,8 +9,8 @@ META_FILE="./metadata"
 CHECKOWN_PATH="./hooks/owner_check"
 VAL_DIR="./validation"
 export ZOWE_APP_SETTINGS_PROJECT_ONLY=true
-export ZOWE_CLI_HOME="$GITHUB_WORKSPACE/.zowe_home"
-export ZOWE_CLI_TELEMETRY_OPTOUT=true
+#export ZOWE_CLI_HOME="$GITHUB_WORKSPACE/.zowe_home"
+#export ZOWE_CLI_TELEMETRY_OPTOUT=true
 
 read_site_cntl()  {
     echo "site file: "$SITE_FILE""
@@ -90,7 +90,7 @@ if ! echo "$COMMIT_MSG_CC" | grep -qE '^[0-9]+$'; then
     exit 1
 fi
 
-rm -rf "$ZOWE_CLI_HOME"
+#rm -rf "$ZOWE_CLI_HOME"
 rm -rf .zowe/
 
 cat << EOF > zowe.config.json
